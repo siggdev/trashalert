5ef26cc66ca1b9be5a314a6cd3be49a9f366f7f0
Christian Bauer <development@bauer-siggenhofen.de>
2021-12-23 15:03:25 +0100

    add seeder for users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if(config('app.debug') == false) {
+            User::factory()->count(10)->create();
+        }
+    }
+}
